4ea609b8f4559da684f5fc150826ddfd8c5ad8d3
rooniecd <124454054+rooniecd@users.noreply.github.com>
2025-10-08 06:16:07 -0400

    Add files via upload

---
+++ docs/config.js
@@ -0,0 +1,7 @@
+// Set this to your deployed Worker origin, e.g.:
+// const API_BASE = "https://roundscript-api.your-subdomain.workers.dev";
+// For local dev with `wrangler dev`, you can also use: const API_BASE = "http://127.0.0.1:8787";
+const API_BASE = "https://roundscript-api.roundscriptdemo.workers.dev";
+
+
+//const API_BASE = "https://roundscript-api.your-subdomain.workers.dev";
